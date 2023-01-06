52f928881772f388fdc793b7814f7ecb9073674e
jeremibiz <110221702+jeremibiz@users.noreply.github.com>
2023-01-06 14:46:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,9 @@
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 from datetime import datetime
-import tensorflow as tf
 
 app = Flask(__name__)
 CORS(app, origins=['http://morphy.fr.to', 'https://morphy.fr.to'])
-
-model = tf.keras.models.load_model('inclusive-writing-model.h5')
-
-def convert_text_to_inclusive_writing(input_text):
-    prediction = model.predict(input_text)
-    return prediction_to_text(prediction)
-
 
 @app.route('/favicon.ico')
 def favicon():
@@ -33,8 +25,7 @@ def status():
 @app.route("/submit", methods=['POST'])
 def submit():
     input_text = request.form['text']
-    output_text = convert_text_to_inclusive_writing(input_text)
-    return "Merci pour votre soumission : " + output_text
+    return "Merci pour votre soumission : " + input_text
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
