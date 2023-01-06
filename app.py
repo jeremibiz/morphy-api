7e48d5196d336fad1e8d2c7fba487255c936b1fe
jeremibiz <110221702+jeremibiz@users.noreply.github.com>
2023-01-06 12:19:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # compose_flask/app.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from datetime import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins=['http://morphy.fr.to', 'https://morphy.fr.to'])
+
+@app.route('/favicon.ico')
+def favicon():
+    return url_for('static', filename='image/favicon.ico')
 
 @app.route("/date")
 def get_current_time():
